8a4525f66b0bc45a91afaf3b0d9f1c5d6c4c6555
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-04-14 19:37:17 +0100

    Add files via upload

---
+++ notes-app/src/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require("body-parser"); 
+const exphbs = require('express-handlebars');
+const path = require('path');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const passport =require('passport');
+
+/*
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+*/
+// Initializations
+const app = express();
+require('./database');
+require('./config/passport');
+
+
+
+
+// settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'mysecretapp',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+app.use(require('./routes/index'));
+app.use(require('./routes/users'));
+app.use(require('./routes/notes'));
+
+
+
+// Server is listening
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+   // console.log('Environment:', process.env.NODE_ENV);
+  });
+
+
+
+  // static files
+app.use(express.static(path.join(__dirname, 'public')));
+module.exports = app;
