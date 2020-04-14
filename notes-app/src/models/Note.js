733df9439eaefe291575da342096c360562ca499
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-04-14 14:17:07 +0100

    Add files via upload

---
+++ notes-app/src/models/Note.js
@@ -0,0 +1,37 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const NoteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: String,
+
+    },
+    date: {
+      type: Date,
+      default:Date.now
+    }
+  },
+  {
+    timestamps: true
+  },{
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+  }
+}
+);
+
+module.exports =mongoose.model("Note", NoteSchema);
+
