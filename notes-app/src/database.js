44279ef18c154a2e51a532c496ea43cd0cfe805e
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-04-15 19:43:09 +0100

    Update database.js

--- notes-app/src/database.js
+++ notes-app/src/database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/notes-db-app',{
 useCreateIndex:true,
 useNewUrlParser: true ,
-useUnifiedTopology: true
-
+useUnifiedTopology: true,
+useFindAndModify:false
 }, (err) => {
     if (!err)
         console.log('MongoDB connection succeeded.');
