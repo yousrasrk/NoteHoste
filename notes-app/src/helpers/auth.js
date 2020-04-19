9af1838ea7a2cfd56bc4e06fd9f4e73b6382040c
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-04-19 12:38:21 +0000

    Add files via upload

--- notes-app/src/helpers/auth.js
+++ notes-app/src/helpers/auth.js
@@ -1,7 +1,9 @@
+
 const helpers = {};
 
 helpers.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
+    res.locals.login = req.isAuthenticated();
     return next();
   }
   console.log('error_msg', 'Not Authorized.');
